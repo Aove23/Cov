7eec618a587fa4b39fdcb7ef434f034a9df35a80
leonidcc <46946252+leonidcc@users.noreply.github.com>
2020-03-20 02:13:06 -0300

    stylov2 y backend

--- backend/addItem.php
+++ backend/addItem.php
@@ -1,13 +1,13 @@
 <?php
 
 if( isset($_POST)){
-  $str_datos = file_get_contents("GAMES.json");
+  $str_datos = file_get_contents("card.json");
   $datos = json_decode($str_datos,true);
 
   $key= substr(md5(time()), 0, 16);
   while(isset($datos[$key]));
   $datos[$key]=$_POST;
-  $fh = fopen("GAMES.json", 'w')
+  $fh = fopen("card.json", 'w')
         or die("Error al abrir fichero de salida");
   fwrite($fh, json_encode($datos,JSON_UNESCAPED_UNICODE));
   fclose($fh);
